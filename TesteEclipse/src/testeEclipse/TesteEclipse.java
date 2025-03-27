1bf9ecee5c55edd5fa3cd13f56384c4e801f2438
Eduardo <Eduardo@Eduardo>
2025-03-26 22:21:46 -0300

    Criação do projeto teste

---
+++ TesteEclipse/src/testeEclipse/TesteEclipse.java
@@ -0,0 +1,17 @@
+package testeEclipse;
+
+import java.util.Scanner;
+
+public class TesteEclipse {
+	
+	public static void main(String[]args) {
+		
+		 Scanner sc = new Scanner(System.in);
+		 
+		 System.out.println("Hello Mundo");
+		 
+		 sc.close();
+	}
+
+	
+}
